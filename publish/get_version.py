08b10e2e7d0ab17951507c78fa73734bb1960f83
iAmSpace <kianraid@gmail.com>
2022-10-20 21:57:20 +0100

    Fixed commit string ingame to automatically change

--- publish/get_version.py
+++ publish/get_version.py
@@ -4,6 +4,12 @@ import os
 
 ver = None
 
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+def get_git_revision_short_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+
 versionPath = os.path.dirname(sys.argv[0]) + "/version.txt"
 if os.path.exists(versionPath):
     with open(versionPath, "r", encoding="utf8") as fp:
@@ -15,5 +21,11 @@ else:
 ver = ver.strip()
 if ver[0] == "v":
     ver = ver[1:]
+
+props = open("src/main/resources/assets/minecraft/client/properties/app.properties",'w') # This file will be accessed in the client - it is useful to set it's values now
+props.write("git.commit.id.abbrev=" + get_git_revision_short_hash() + "\n")
+props.write("git.commit.id=" + get_git_revision_hash() + "\n")
+props.write("git.branch=master")
+props.close()
 
 print(ver)
